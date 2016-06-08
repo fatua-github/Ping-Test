b01445da323f7eb23402113813354b5ab300ed95
Jeff Mathers <jmathers@probus.ca>
2016-06-08 09:20:07 -0400

    Fixed date handling in file nam e

--- startpings.ps1
+++ startpings.ps1
@@ -3,9 +3,9 @@
 $Computers = @("127.0.0.1","www.google.com","www.google.ca","invalid")
 $ScriptPath = ".\ping-test.ps1"
 $Count = 5 # = 86400 =24 hours in seconds (with perfect pings = 1 day)
-
+$date = get-date -Format yyyy-MM-dd
 foreach ($computer in $computers)  {
-    $logpath = ".\$(get-date -Format yyyy-MM-dd)-$Computer-Pinglog.csv"
+    $logpath = ".\$date -$Computer-Pinglog.csv"
     $Arguments = "$Scriptpath -Computer $computer -count $Count -LogPath $logpath -Verbose" 
     write-host $Arguments
     start-Process -filepath powershell -ArgumentList $Arguments
