5b7c47d0d0720eda20a15031668f42e1a8d919ab
fatua-github <github@fatua.cA>
2016-06-13 11:41:34 -0400

    Updated script to be compatible with Powershell 3.0+ and added TCP connect test and latency timer.

--- ping-test.ps1
+++ ping-test.ps1
@@ -7,6 +7,8 @@
 
 
 # June 7th, 2016 - Initial Creation
+# June 13th, 2016 - Added TCP tests
+#  - Cannot use test-netconnection as this requires powershell 4.0.  My client base uses powershell v3.0
 
 
 
@@ -42,31 +44,51 @@ If (-not (Test-Path (Split-Path $LogPath) -PathType Container))
 #Test if log file exists, if not seed it with a header row
 If (-not (Test-Path $LogPath))
 {   Write-Verbose "Log file doesn't exist: $($LogPath), creating..."
-    Add-Content -Value '"TimeStamp","Source","Destination","IPV4Address","Status","ResponseTime"' -Path $LogPath
+    Add-Content -Value '"TimeStamp","Source","Destination","IPV4Address","Status","ResponseTime","TCPRemotePort","TCPTestSucceeded","TCPTime-ms"' -Path $LogPath
 }
 
 #Log collection loop
-if ($ComputerPORT) {
+if ($ComputerPORT) { #If port specified, do a TCP test
     $output = New-Object System.Object
     While ($count -gt 0){
         #Fill in TCP Ping details
         Write-Verbose "Beginning TCP moniotring of $Computer on Port $ComputerPORT. $count tests remaining"
         $output | Add-Member -type NoteProperty -name Timestamp -Value $(Get-Date)
-        $time = Measure-Command {$result = test-netconnection -ComputerName $Computer -Port $ComputerPORT -InformationLevel Detailed}
-        write-Verbose "Time: $Time"
-        $output | Add-Member -type NoteProperty -name Source -Value $env:ComputerName
-        $output | Add-Member -type NoteProperty -name Destination -Value $result.ComputerName
-        $output | Add-Member -type NoteProperty -name IPV4Address -Value $result.RemoteAddress
-        $output | Add-Member -type NoteProperty -name Status -Value $result.PingSucceeded
-        $output | Add-Member -type NoteProperty -name ResponseTime -Value $result.PingReplyDetails
-        $output | Add-Member -type NoteProperty -name TCPRemotePort -Value $result.RemotePort
-        $output | Add-Member -type NoteProperty -name TCPTestSucceeded -Value $result.TCPTestSucceeded
-        $output | Add-Member -type NoteProperty -name TimeMeasure -Value $time
-  #      $output.RemotePort = $result.RemotePort
+ #      $time = Measure-Command {$result = test-netconnection -ComputerName $Computer -Port $ComputerPORT -InformationLevel Detailed}   # test-netconnection not available on powershell 3.0
+        $test = New-Object System.Net.Sockets.TcpClient  #Create TCP connection object
 
-        write-Verbose ($Result | Format-Table -AutoSize | Out-String)
-        Write-Verbose ($output | Format-Table -AutoSize | Out-String)
-        $count--
+        ### Perform TCP Test
+        Try{
+            $time = Measure-Command {$test.Connect($Computer, $ComputerPORT)} #Connect and measure
+            $TCPStatus = $true
+        }
+        Catch{
+            $time = 9999999
+            $TCPStatus = $false
+        }
+        Finally{
+            $test.Dispose()
+        }
+        ### Perform ICMP test
+        $Ping = Get-WmiObject Win32_PingStatus -Filter "Address = '$Computer'" | Select @{Label="TimeStamp";Expression={Get-Date}},@{Label="Source";Expression={ $_.__Server }},@{Label="Destination";Expression={ $_.Address }},IPv4Address,@{Label="Status";Expression={ If ($_.StatusCode -ne 0) {"Failed"} Else {""}}},ResponseTime
+        $Result = $Ping | Select TimeStamp,Source,Destination,IPv4Address,Status,ResponseTime
+        if ($Result.status -eq "Failed" ) {
+           $Result.ResponseTime = 9999999
+        }
+        $output | Add-Member -type NoteProperty -name Source -Value $env:ComputerName
+        $output | Add-Member -type NoteProperty -name Destination -Value $result.Destination
+        $output | Add-Member -type NoteProperty -name IPV4Address -Value $result.IPV4Address
+        $output | Add-Member -type NoteProperty -name Status -Value $result.Status
+        $output | Add-Member -type NoteProperty -name ResponseTime -Value $result.Responsetime
+        $output | Add-Member -type NoteProperty -name TCPRemotePort -Value $ComputerPORT
+        $output | Add-Member -type NoteProperty -name TCPTestSucceeded -Value $TCPStatus
+        $output | Add-Member -type NoteProperty -name TimeMeasure -Value $($time.TotalMilliseconds)
+        
+       Write-Verbose ($output | Format-Table -AutoSize | Out-String)
+       $outputCSV = $output | ConvertTo-Csv -NoTypeinformation
+       Start-Sleep -Seconds 1
+       $count--
+       $OutputCSV[1] | Add-Content -Path $LogPath
     }
 }
 else {
